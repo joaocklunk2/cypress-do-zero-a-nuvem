d214b39ca8262b042d10aa7352105f5452decc5f
joaocklunk2 <joaocklunk@gmail.com>
2026-01-03 13:36:41 -0300

    Primeiro teste escrito com Cypres - Secao 2 aula 6

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -1,5 +1,11 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+//O bloco describe define a suite de testes e o bloco it define o caso de teste.
+
+describe('Central de Atendimento ao Cliente TAT', () => {
+  it('verifica o título da aplicação', () => {
+    cy.visit('./src/index.html')
+    cy.title('Central de Atendimento ao Cliente TAT')
+    cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
+    cy.title().should('include', 'Cliente')
+
   })
 })
