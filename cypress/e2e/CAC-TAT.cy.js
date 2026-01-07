558b40f8a6733c32f52daf65035506a6f9250fef
joaocklunk2 <joaocklunk@gmail.com>
2026-01-06 21:57:47 -0300

    Realizacao ate o exec. extra6

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -10,7 +10,7 @@ describe('Central de Atendimento ao Cliente TAT', () => {
     cy.title().should('include', 'Cliente')
   })
 
-   it.only('preenche os campos obrigatórios e envia o formulário', () => {
+   it('preenche os campos obrigatórios e envia o formulário', () => {
     /*MINHA SOLUÇÃO
     cy.get('input[name="firstName"]')
     .as('firstName')
@@ -47,5 +47,70 @@ describe('Central de Atendimento ao Cliente TAT', () => {
 
     cy.get('.success > strong').should('be.visible')
   })
+
+    it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', () => {
+    const longText = Cypress._.repeat('Joao', 100)
+    cy.get('#firstName').type('João')
+    cy.get('#lastName').type('Klunk')
+    cy.get('#email').type('joaoteste')
+    cy.get('#open-text-area').type(longText, {delay: 0})
+    cy.get('button[type="submit"]').click()
+
+    cy.get('.error > strong').should('be.visible')
+  })
+
+    it('valida se o campo de telefone está vazio quando preenchido com um valor não numérico', () => {
+    cy.get('#phone')
+      .type('joaoteste')
+      .should('have.value', '') //verifica se tem um valor no campo 
+  })
+
+  it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', () => {
+    cy.get('#firstName').type('João')
+    cy.get('#lastName').type('Klunk')
+    cy.get('#email').type('joao@email.com')
+    cy.get('#phone-checkbox').click()
+    cy.get('#open-text-area').type('teste')
+    cy.get('button[type="submit"]').click()
+
+    cy.get('.error > strong').should('be.visible')
+  })
+
+  it('preenche e limpa os campos nome, sobrenome, email e telefone', () => {
+    cy.get('#firstName')
+      .type('João')
+      .should('have.value', 'João')
+      .clear()
+      .should('have.value', '')
+    cy.get('#lastName')
+      .type('Klunk')
+      .should('have.value', 'Klunk')
+      .clear()
+      .should('have.value', '')
+    cy.get('#email')
+      .type('joao@email.com')
+      .should('have.value', 'joao@email.com')
+      .clear()
+      .should('have.value', '')
+    cy.get('#phone')
+      .type('51984183158')
+      .should('have.value', '51984183158')
+      .clear()
+      .should('have.value', '')   
+  })
+
+  it.only('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', () => {
+    cy.get('button[type="submit"]')
+      .click()
+    cy.get('.error > strong')
+      .should('be.visible')
+  })
+
+  
+
+
+
+
+  
 })
 
